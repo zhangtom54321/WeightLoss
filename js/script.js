d756c15734179d1ff5a38458b1d2cd0cd061b662
Herman Saini <herman.saini@gmail.com>
2017-06-24 15:51:29 -0700

    asdf;laksd

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ window.onload = function(){
   var reference = database.ref()
   var submitButton = document.getElementById("vive-button5");
   submitButton.onclick = function(){
+    alert("We have recieved you inf0!");
     var emailValue = document.getElementById("emailEnter").value;
     var cityValue = document.getElementById("emailEnter2").value;
     var usernameValue = document.getElementById("emailEnter3").value;
 
-    reference.push({ email: emailValue, city: cityValue, username: usernameValue })
+    // reference.push({ email: emailValue, city: cityValue, username: usernameValue })
   };
 
   function sendMail(){
