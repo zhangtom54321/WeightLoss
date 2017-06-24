cc687b93ff0010a62899dfa31f1705c986ade362
Herman Saini <herman.saini@gmail.com>
2017-06-24 14:52:03 -0700

    Firebase Setup

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-'keydown input[type="text"]': function(event, template) {
-    if ((27 === event.which) || (13 === event.which)) {
-        event.preventDefault();
-        //this should delete value from the input
-        event.currentTarget.value = "";
-    }
+window.onload = function(){
+  var database = firebase.database()
+  var reference = database.ref()
+  var submitButton = document.getElementById("vive-button5");
+
+  submitButton.onclick = function(){
+    var emailValue = document.getElementById("emailEnter").value;
+    var cityValue = document.getElementById("emailEnter2").value;
+    var usernameValue = document.getElementById("emailEnter3").value;
+
+    reference.push({ email: emailValue, city: cityValue, username: usernameValue })
+  };
 }
